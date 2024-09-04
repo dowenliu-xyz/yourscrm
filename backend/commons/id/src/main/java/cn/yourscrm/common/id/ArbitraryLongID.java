67c5607d7f6e5ca1beec189d7b24383505b3d3d1
dowenliu-xyz <hawkdowen@hotmail.com>
2024-09-04 14:44:16 +0800

    refactor(id): ArbitraryLongID

---
+++ backend/commons/id/src/main/java/cn/yourscrm/common/id/ArbitraryLongID.java
@@ -0,0 +1,38 @@
+package cn.yourscrm.common.id;
+
+import java.util.Objects;
+
+public class ArbitraryLongID implements ID {
+    private final long id;
+
+    public ArbitraryLongID(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String asString() {
+        return "" + id;
+    }
+
+    @Override
+    public long asLong() {
+        return id;
+    }
+
+    @Override
+    public boolean canBeLong() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ID oid)) return false;
+        return Objects.equals(asString(), oid.asString());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(asString());
+    }
+}
